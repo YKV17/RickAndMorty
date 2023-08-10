113b037d23c83cb8bf2c9f8dfb27e42c3af7ec43
ykv17 <yogeshvrma17@gmail.com>
2023-08-10 23:57:30 +0530

    Added dependencies from kotlin DSL

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,14 @@
+object Dependencies {
+    object CoreDep {
+        val coreKtx = "androidx.core:core-ktx:${Versions.coreKtxVersion}"
+        val appCompat = "androidx.appcompat:appcompat:${Versions.appCompatVersion}"
+        val material = "com.google.android.material:material:${Versions.materialVersion}"
+        val constraint = "androidx.constraintlayout:constraintlayout:${Versions.constraintVersion}"
+    }
+
+    object TestDep {
+        var junit = "junit:junit:${Versions.junitVersion}"
+        var testJunitExt = "androidx.test.ext:junit:${Versions.testJunitExtVersion}"
+        var espressoCore = "androidx.test.espresso:espresso-core:${Versions.espressoCoreVersion}"
+    }
+}
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,2 +1,12 @@
 object Versions {
+    val coreKtxVersion = "1.10.1"
+    val appCompatVersion = "1.6.1"
+    val materialVersion = "1.9.0"
+    val constraintVersion = "2.1.4"
+
+    val junitVersion = "4.13.2"
+    val testJunitExtVersion = "1.1.5"
+    val espressoCoreVersion = "3.5.1"
+
+
 }
